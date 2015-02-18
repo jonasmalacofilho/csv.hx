00ef8e9dc38c6cc3585ec321dbf198f32cb00ce0
Jonas Malaco Filho <jonas@jonasmalaco.com>
2015-02-18 02:14:47 -0200

    Utf8 & streaming reader: validate read data first

--- lib/format/csv/rdp/Utf8Parser.hx
+++ lib/format/csv/rdp/Utf8Parser.hx
@@ -9,20 +9,15 @@ class Utf8Parser extends Parser {
         var bpos = p - bufferOffset;
         if (bpos + len > Utf8.length(buffer)) {
             var more = readMore(4);
+            while (more != null && !Utf8.validate(more))
+                more += readMore(1);
             if (more != null) {
                 buffer = Utf8.sub(buffer, pos - bufferOffset, Utf8.length(buffer)) + more;
                 bufferOffset = pos;
                 bpos = p - bufferOffset;
             }
         }
-        var ret = null;
-        while (ret == null) {
-            try {
-                ret = Utf8.sub(buffer, bpos, len);
-            } catch (e:Dynamic) {
-                buffer += inp.read(1).toString();
-            }
-        }
+        var ret = Utf8.sub(buffer, bpos, len);
         return ret != "" ? ret : null;
     }
 
